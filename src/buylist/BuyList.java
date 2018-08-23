82c92e3daddce7fc2351e41077e6fcede41447ed
gusmendez99 <gus.mendez.99@gmail.com>
2018-08-22 23:06:05 -0600

    Display List of Lists

--- src/buylist/BuyList.java
+++ src/buylist/BuyList.java
@@ -1,17 +1,29 @@
 package buylist;
 
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import product.Product;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
-public class BuyList {
+public class BuyList extends RecursiveTreeObject<BuyList> {
+    SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
     SimpleStringProperty name;
-    private ArrayList products;
+    SimpleObjectProperty<String> date;
+    private ArrayList<Product> products;
+
 
     public BuyList(String name){
         this.name = new SimpleStringProperty(name);
         this.products = new ArrayList<>();
+        this.date = new SimpleObjectProperty<String>(sdf.format(new Date()));
     }
 
     public String getName() {
@@ -22,11 +34,37 @@ public class BuyList {
         this.name.set(name);
     }
 
-    public ArrayList getProducts() {
-        return products;
+    public SimpleStringProperty nameProperty() {
+        return name;
+    }
+
+    public SimpleObjectProperty dateProperty() {
+        return date;
+    }
+
+    public SimpleIntegerProperty pendingProductsProperty(){
+        return new SimpleIntegerProperty(getPendingProducts().size());
+    }
+
+    public SimpleDoubleProperty estimatePendingProperty(){
+        return new SimpleDoubleProperty(getEstimatePendingTotal());
+    }
+
+    public List<Product> getPendingProducts(){
+        return this.products.stream()             // convert list to stream
+                .filter(p -> !p.isComplete())     // we dont like not pendings
+                .collect(Collectors.toList());
+    }
+
+    public double getEstimatePendingTotal(){
+        return this.products.stream()             // convert list to stream
+                .filter(p -> !p.isComplete())
+                .mapToDouble(Product::getPrice)
+                .sum();
     }
 
     public void addProduct(Product product) {
         this.products.add(product);
     }
+
 }
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,69 @@
 package sample;
 
+import buylist.BuyList;
+import com.jfoenix.controls.JFXTreeTableColumn;
+import com.jfoenix.controls.JFXTreeTableView;
+import com.jfoenix.controls.RecursiveTreeItem;
+import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.function.Function;
+
 public class Controller {
 
+    //Properties
+    @FXML
+    private JFXTreeTableView<BuyList> buyListTable;
+
+    @FXML
+    private JFXTreeTableColumn<BuyList, String> nameColumn;
+
+    @FXML
+    private JFXTreeTableColumn<BuyList, LocalDate> dateColumn;
+
+    @FXML
+    private JFXTreeTableColumn<BuyList, Integer> pendingColumn;
+
+    @FXML
+    private JFXTreeTableColumn<BuyList, Double> estimateColumn;
+
+    @FXML
+    public void initialize() {
+
+        DateTimeFormatter myDateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
+        ObservableList<BuyList> data = FXCollections.observableArrayList(
+                new BuyList("ToDos"),
+                new BuyList("Amazon Wish List")
+        );
+
+        setupCellValueFactory(nameColumn, BuyList::nameProperty);
+        setupCellValueFactory(dateColumn, BuyList::dateProperty);
+        setupCellValueFactory(pendingColumn, bl -> bl.pendingProductsProperty().asObject());
+        setupCellValueFactory(estimateColumn, bl -> bl.estimatePendingProperty().asObject());
+
+        buyListTable.setRoot(new RecursiveTreeItem<>(data, RecursiveTreeObject::getChildren));
+        buyListTable.setShowRoot(false);
+
+    }
+
+    private <T> void setupCellValueFactory(JFXTreeTableColumn<BuyList, T> column, Function<BuyList, ObservableValue<T>> mapper) {
+        column.setCellValueFactory((TreeTableColumn.CellDataFeatures<BuyList, T> param) -> {
+            if (column.validateValue(param)) {
+                return mapper.apply(param.getValue().getValue());
+            } else {
+                return column.getComputedValue(param);
+            }
+        });
+    }
 
 
 
